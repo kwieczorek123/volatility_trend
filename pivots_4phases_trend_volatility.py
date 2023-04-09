c39f118d99d207d34f5ff63164e840ab6ba3575f
formaaaa <wieczorek.konrad@gmail.com>
2023-04-09 23:17:11 +0300

    amended the pivots for 4phases and 8phases to make calculations for multiple intervals and take the execution spread and PnL data from common files for each interval from SQL query

--- pivots_4phases_trend_volatility.py
+++ pivots_4phases_trend_volatility.py
@@ -7,24 +7,16 @@ from openpyxl.styles import Font
 from openpyxl.utils import get_column_letter
 
 # Define file names for input OHLC_data
-file_names = ['results/trend_volatility_results/processed_CI_data/EURUSD_CI.csv', 'results/trend_volatility_results'
-                                                                                  '/processed_CI_data/GBPUSD_CI.csv',
-              'results/trend_volatility_results/processed_CI_data/USDJPY_CI.csv',
-              'results/trend_volatility_results/processed_CI_data/XAUUSD_CI.csv']
+# Daily data for _long pivots generated from running the Choppiness_Index.py file
+file_names_pivots_long = ['results/trend_volatility_results/processed_CI_data/EURUSD_CI_D1.csv',
+                          'results/trend_volatility_results/processed_CI_data/GBPUSD_CI_D1.csv',
+                          'results/trend_volatility_results/processed_CI_data/USDJPY_CI_D1.csv',
+                          'results/trend_volatility_results/processed_CI_data/XAUUSD_CI_D1.csv']
 spread_all_symbols_file = 'Input data/trend_volatility_input_data/spread_all_symbol.csv'
-execution_spread_file = 'Input data/trend_volatility_input_data/execution_spread.csv'
-pnl_files = ['Input data/trend_volatility_input_data/EURUSD_pnl.csv', 'Input data/trend_volatility_input_data'
-                                                                      '/GBPUSD_pnl.csv',
-             'Input data/trend_volatility_input_data/USDJPY_pnl.csv',
-             'Input data/trend_volatility_input_data/XAUUSD_pnl.csv']
-
-# Create a dictionary to store PnL OHLC_data for each symbol
-pnl_df_dict = {symbol: pd.read_csv(pnl_file, parse_dates=['day']) for pnl_file, symbol in
-               zip(pnl_files, ['EURUSD', 'GBPUSD', 'USDJPY', 'XAUUSD'])}
 
 # Define date range and rolling window length
-start_date = '2023-01-01'
-end_date = '2023-03-30'
+start_date = '2020-01-01'
+end_date = '2023-03-31'
 roll = 252
 start_date_long = '2015-01-01'
 end_date_long = '2022-12-31'
@@ -62,7 +54,7 @@ def mark_volatility_trend_actionable(df):
     return df
 
 
-# Function to add spread columns to the input DataFrame
+# Function to add typical_spread, and execution spread columns to the input DataFrame
 def add_spread_columns(df, spread_df, execution_df, symbol):
     spread_factors = {'EURUSD': 100000, 'GBPUSD': 100000, 'USDJPY': 1000, 'XAUUSD': 100}
 
@@ -73,18 +65,18 @@ def add_spread_columns(df, spread_df, execution_df, symbol):
     # Multiply the typical_spread column by the appropriate factor
     df['typical_spread_in_points'] = df['typical_spread_in_points'] * spread_factors[symbol]
 
-    df = df.merge(execution_df.loc[execution_df['symbol_name'] == symbol, ['date', 'vol_avg_spread']], on='date',
-                  how='left')
-    df = df.rename(columns={'vol_avg_spread': 'weighted_avg_execution_spread_$'})
+    df = df.merge(execution_df.loc[execution_df['symbol_name'] == symbol, ['date', 'vol_avg_spread', 'volume']],
+                  on='date', how='left')
+    df = df.rename(columns={'vol_avg_spread': 'weighted_avg_execution_spread_$', 'volume': 'volume_spreads_df'})
 
     return df
 
 
-# Modify the add_pnl_per_lot_column function to add the 'profit' and 'volume' columns
+# Function to add profit, volume and PnL_per_lot columns to the input DataFrame
 def add_pnl_per_lot_column(df, pnl_df, symbol):
-    df = df.merge(pnl_df.loc[pnl_df['symbol_name'] == symbol, ['day', 'PnL/Lot', 'profit', 'volume']],
+    df = df.merge(pnl_df.loc[pnl_df['symbol_name'] == symbol, ['day', 'PnL_per_lot', 'profit', 'volume']],
                   left_on='date', right_on='day', how='left')
-    df = df.rename(columns={'PnL/Lot': 'PnL_per_lot', 'profit': 'total_profit', 'volume': 'total_volume'})
+    df = df.rename(columns={'profit': 'total_profit', 'volume': 'total_volume'})
     df.drop(columns=['day'], inplace=True)
     return df
 
@@ -102,7 +94,7 @@ def process_csv(file_name, start_date, end_date, spread_df, execution_df, pnl_df
     output_dir = "results/trend_volatility_results/processed_4phases_data"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    output_file_name = os.path.basename(file_name).replace("_CI", "_vol_trend")  # Replace '_chop' with '_vol_trend'
+    output_file_name = os.path.basename(file_name).replace("_CI", "_vol_trend")
     output_file_path = os.path.join(output_dir, f'processed_{output_file_name}')
     df.to_csv(output_file_path, index=False)
     print(f"Processed {file_name}")
@@ -124,20 +116,13 @@ def process_csv_long(file_name, start_date, end_date):
     print(f"Processed {file_name}")
 
 
-# Read input OHLC_data from the spread and execution files
-spread_df = pd.read_csv(spread_all_symbols_file, parse_dates=['day'])
-execution_df = pd.read_csv(execution_spread_file, parse_dates=['date'])
-
-# Process each input file
-for file_name in file_names:
-    process_csv(file_name, start_date, end_date, spread_df, execution_df, pnl_df_dict)
-
-for file_name in file_names:
+# Process each input file for _long pivots
+for file_name in file_names_pivots_long:
     process_csv_long(file_name, start_date_long, end_date_long)
 
 
 # Function to create pivot tables and charts using the processed OHLC_data
-def create_pivot_tables_and_charts(processed_files):
+def create_pivot_tables_and_charts(processed_files, interval):
     processed_dfs = [pd.read_csv(file) for file in processed_files]
 
     combined_df = pd.concat(processed_dfs, keys=['EURUSD', 'GBPUSD', 'USDJPY', 'XAUUSD'], names=['symbol']).reset_index(
@@ -159,13 +144,12 @@ def create_pivot_tables_and_charts(processed_files):
         # Calculate the weighted_spread_by_volume column
         symbol_df = symbol_df.copy()  # Create a copy of the DataFrame to avoid SettingWithCopyWarning
         symbol_df['weighted_spread_by_volume'] = symbol_df['weighted_avg_execution_spread_$'] * symbol_df[
-            'total_volume']
+            'volume_spreads_df']
 
         pivot = pd.pivot_table(symbol_df, index='Volatility_Trend',
                                values=['typical_spread_in_points', 'weighted_avg_execution_spread_$',
                                        'PnL_per_lot', 'total_profit', 'total_volume', 'weighted_spread_by_volume'],
                                aggfunc={'typical_spread_in_points': 'mean',
-                                        'weighted_avg_execution_spread_$': 'mean',
                                         'PnL_per_lot': ['mean', 'count'],
                                         'total_profit': 'sum',
                                         'total_volume': 'sum',
@@ -205,9 +189,8 @@ def create_pivot_tables_and_charts(processed_files):
         # Insert the new columns after total_profit and total_volume
         pivot = pivot.reindex(
             columns=['count_of_occurrences', 'percentage_of_occurrences', 'typical_spread_in_points',
-                     'weighted_avg_execution_spread_$', 'volume_weighted_avg_spread_in_USD', 'PnL_per_lot',
-                     'total_profit', 'pct_total_profit', 'total_volume', 'pct_total_volume',
-                     'pct_impact_on_PnL_exec_spread'])
+                     'volume_weighted_avg_spread_in_USD', 'PnL_per_lot', 'total_profit', 'pct_total_profit',
+                     'total_volume', 'pct_total_volume', 'pct_impact_on_PnL_exec_spread'])
 
         row_offset += 1
 
@@ -222,11 +205,14 @@ def create_pivot_tables_and_charts(processed_files):
             length = max(len(str(cell.value)) for cell in column_cells)
             ws.column_dimensions[get_column_letter(column_cells[0].column)].width = length + 2
 
-        wb.save("results/trend_volatility_results/processed_4phases_data/pivots_4phases.xlsx")
+        output_dir = f"results/trend_volatility_results/processed_4phases_data/pivots"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        wb.save(f"{output_dir}/pivots_4phases_{interval}.xlsx")
 
 
-# Function to create pivot tables and charts using the processed OHLC_data
-def create_pivot_tables_and_charts_actionable(processed_files):
+# Function to create pivot tables using the processed OHLC_data
+def create_pivot_tables_and_charts_actionable(processed_files, interval):
     processed_dfs = [pd.read_csv(file) for file in processed_files]
 
     combined_df = pd.concat(processed_dfs, keys=['EURUSD', 'GBPUSD', 'USDJPY', 'XAUUSD'], names=['symbol']).reset_index(
@@ -248,13 +234,12 @@ def create_pivot_tables_and_charts_actionable(processed_files):
         # Calculate the weighted_spread_by_volume column
         symbol_df = symbol_df.copy()  # Create a copy of the DataFrame to avoid SettingWithCopyWarning
         symbol_df['weighted_spread_by_volume'] = symbol_df['weighted_avg_execution_spread_$'] * symbol_df[
-            'total_volume']
+            'volume_spreads_df']
 
         pivot = pd.pivot_table(symbol_df, index='Volatility_Trend_actionable',
                                values=['typical_spread_in_points', 'weighted_avg_execution_spread_$',
                                        'PnL_per_lot', 'total_profit', 'total_volume', 'weighted_spread_by_volume'],
                                aggfunc={'typical_spread_in_points': 'mean',
-                                        'weighted_avg_execution_spread_$': 'mean',
                                         'PnL_per_lot': ['mean', 'count'],
                                         'total_profit': 'sum',
                                         'total_volume': 'sum',
@@ -294,9 +279,8 @@ def create_pivot_tables_and_charts_actionable(processed_files):
         # Insert the new columns after total_profit and total_volume
         pivot = pivot.reindex(
             columns=['count_of_occurrences', 'percentage_of_occurrences', 'typical_spread_in_points',
-                     'weighted_avg_execution_spread_$', 'volume_weighted_avg_spread_in_USD', 'PnL_per_lot',
-                     'total_profit', 'pct_total_profit', 'total_volume', 'pct_total_volume',
-                     'pct_impact_on_PnL_exec_spread'])
+                     'volume_weighted_avg_spread_in_USD', 'PnL_per_lot', 'total_profit', 'pct_total_profit',
+                     'total_volume', 'pct_total_volume', 'pct_impact_on_PnL_exec_spread'])
 
         row_offset += 1
 
@@ -311,7 +295,10 @@ def create_pivot_tables_and_charts_actionable(processed_files):
             length = max(len(str(cell.value)) for cell in column_cells)
             ws.column_dimensions[get_column_letter(column_cells[0].column)].width = length + 2
 
-        wb.save("results/trend_volatility_results/processed_4phases_data/pivots_4phases_actionable.xlsx")
+        output_dir = f"results/trend_volatility_results/processed_4phases_data/pivots"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        wb.save(f"{output_dir}/pivots_4phases_actionable_{interval}.xlsx")
 
 
 def create_pivot_tables_and_charts_long(processed_files):
@@ -366,27 +353,57 @@ def create_pivot_tables_and_charts_long(processed_files):
             length = max(len(str(cell.value)) for cell in column_cells)
             ws.column_dimensions[get_column_letter(column_cells[0].column)].width = length + 2
 
-    wb.save("results/trend_volatility_results/processed_4phases_data/pivots_4phases_long.xlsx")
+    wb.save("results/trend_volatility_results/processed_4phases_data/pivots/pivots_4phases_long.xlsx")
 
 
-# Add this line to the end of your script to call the function
-create_pivot_tables_and_charts([
-    "results/trend_volatility_results/processed_4phases_data/processed_EURUSD_vol_trend.csv",
-    "results/trend_volatility_results/processed_4phases_data/processed_GBPUSD_vol_trend.csv",
-    "results/trend_volatility_results/processed_4phases_data/processed_USDJPY_vol_trend.csv",
-    "results/trend_volatility_results/processed_4phases_data/processed_XAUUSD_vol_trend.csv"
-])
+# Add a new function to process all intervals
+def process_all_intervals(interval_list):
+    for interval in interval_list:
+        file_names = [f"results/trend_volatility_results/processed_CI_data/{symbol}_CI_{interval}.csv" for symbol in
+                      ['EURUSD', 'GBPUSD', 'USDJPY', 'XAUUSD']]
 
-create_pivot_tables_and_charts_actionable([
-    "results/trend_volatility_results/processed_4phases_data/processed_EURUSD_vol_trend.csv",
-    "results/trend_volatility_results/processed_4phases_data/processed_GBPUSD_vol_trend.csv",
-    "results/trend_volatility_results/processed_4phases_data/processed_USDJPY_vol_trend.csv",
-    "results/trend_volatility_results/processed_4phases_data/processed_XAUUSD_vol_trend.csv"
-])
+        # Update the execution spread and pnl files for the current interval
+        execution_spread_file = f'Input data/trend_volatility_input_data/execution_spread_{interval}.csv'
+        pnl_file = f'Input data/trend_volatility_input_data/PnL_data_{interval}.csv'
 
+        # Read input data from the spread and execution files
+        spread_df = pd.read_csv(spread_all_symbols_file, parse_dates=['day'])
+        execution_df = pd.read_csv(execution_spread_file, parse_dates=['date'])
+
+        # Create a dictionary to store PnL data for each symbol
+        pnl_df = pd.read_csv(pnl_file, parse_dates=['day'])
+        pnl_df_dict = {symbol: pnl_df[pnl_df['symbol_name'] == symbol] for symbol in
+                       ['EURUSD', 'GBPUSD', 'USDJPY', 'XAUUSD']}
+
+        # Create a list to store processed file names
+        processed_file_names = []
+
+        for file_name in file_names:
+            # Call the process_csv function to process the data and save the processed file
+            process_csv(file_name, start_date, end_date, spread_df, execution_df, pnl_df_dict)
+
+            # Create the processed file name and append it to the list
+            output_file_name = os.path.basename(file_name).replace("_CI", "_vol_trend")
+            output_file_path = os.path.join("results/trend_volatility_results/processed_4phases_data",
+                                            f'processed_{output_file_name}')
+            processed_file_names.append(output_file_path)
+
+        # Call the functions with the processed_file_names
+        create_pivot_tables_and_charts(processed_file_names, interval)
+        create_pivot_tables_and_charts_actionable(processed_file_names, interval)
+
+
+# Call the process_all_intervals function with the desired interval values
+interval = 'all'  # Change this to 'D1', 'H1', or 'all'
+if interval == 'all':
+    process_all_intervals(['D1', 'H1'])
+else:
+    process_all_intervals([interval])
+
+# Call the create_pivot_tables_and_charts_long function with fixed interval='D1'
 create_pivot_tables_and_charts_long([
-    "results/trend_volatility_results/processed_4phases_data/processed_EURUSD_vol_trend_long.csv",
-    "results/trend_volatility_results/processed_4phases_data/processed_GBPUSD_vol_trend_long.csv",
-    "results/trend_volatility_results/processed_4phases_data/processed_USDJPY_vol_trend_long.csv",
-    "results/trend_volatility_results/processed_4phases_data/processed_XAUUSD_vol_trend_long.csv"
+    "results/trend_volatility_results/processed_4phases_data/processed_EURUSD_vol_trend_long_D1.csv",
+    "results/trend_volatility_results/processed_4phases_data/processed_GBPUSD_vol_trend_long_D1.csv",
+    "results/trend_volatility_results/processed_4phases_data/processed_USDJPY_vol_trend_long_D1.csv",
+    "results/trend_volatility_results/processed_4phases_data/processed_XAUUSD_vol_trend_long_D1.csv"
 ])
